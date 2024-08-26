742e291b67ef476cbeb966e5ed6082e87e3f09ca
Ahmed Abdelwahed <ahmed.abdelwahed.dev@gmail.com>
2024-08-26 22:36:33 +0300

    Advanced React Design Patterns

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,4 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
-export { Header, Footer };
+import GridList from "./gridList/GridList";
+export { Header, Footer, GridList };
